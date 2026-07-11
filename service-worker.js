bca4ec0b15f8434e00b8309c71f9393e11693b46
m249gjhn2s-art <m249gjhn2s@privaterelay.appleid.com>
2026-07-11 12:58:34 -0400

    Create service-worker.js
    app stuff

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "ct-youth-camp-2026";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest"
+];
+
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => cache.addAll(FILES))
+  );
+});
+
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => response || fetch(event.request))
+  );
+});
